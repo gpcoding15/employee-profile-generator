b25a9d13aae5623b993da0b6388e996a47b6a935
gpcoding15 <gisela.pardini15@gmail.com>
2026-06-23 01:23:38 -0300

    address variable created

--- employee-profile-generator.py
+++ employee-profile-generator.py
@@ -1,6 +1,8 @@
 first_name = 'John'
 last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = "123 Main Street"
 print(first_name)
 print(last_name)
-full_name = first_name + " " + last_name
 print(full_name)
+print(address)
