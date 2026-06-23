2bf994e01a3bc9270564c56b082782a09523864d
gpcoding15 <gisela.pardini15@gmail.com>
2026-06-23 03:26:36 -0300

    department added

--- employee-profile-generator.py
+++ employee-profile-generator.py
@@ -9,4 +9,10 @@ print(employee_info)
 experience_years = 5
 experience_info = 'Experience: ' + str(experience_years) + ' years'
 print(experience_info)
-employee_card = f'Employee: {full_name} | Age: {employee_age}'
+position = 'Data Analyst'
+salary = 75000
+employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
+print(employee_card)
+employee_code = "DEV-2026-JD-001"
+department = employee_code[0:3]
+print(department)
