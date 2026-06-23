6713d058da2671cd9499708d2102b476f40f47bc
gpcoding15 <gisela.pardini15@gmail.com>
2026-06-23 01:58:20 -0300

    employee_info updated

--- employee-profile-generator.py
+++ employee-profile-generator.py
@@ -1,5 +1,7 @@
-first_name = 'John'  
-last_name = 'Doe'  
-full_name = first_name + ' ' + last_name   
-address = '123 Main Street'  
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+address = '123 Main Street'
 address += ', Apartment 4B'
+employee_age = 28
+employee_info = full_name + ' is ' + str(employee_age)
