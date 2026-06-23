ed6a04ac671c42797cfc08d901fcb5e76ecde8b0
gpcoding15 <gisela.pardini15@gmail.com>
2026-06-23 03:20:32 -0300

    experience_info added

--- employee-profile-generator.py
+++ employee-profile-generator.py
@@ -4,4 +4,8 @@ full_name = first_name + ' ' + last_name
 address = '123 Main Street'
 address += ', Apartment 4B'
 employee_age = 28
-employee_info = full_name + ' is ' + str(employee_age)
+employee_info = full_name + ' is ' + str(employee_age) + ' years old'
+print(employee_info)
+experience_years = 5
+experience_info = "Experience: " + str(experience_years) + " years"
+print(experience_info)
