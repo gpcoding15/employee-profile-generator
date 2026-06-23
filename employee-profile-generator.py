42c3a2dd688aaafb91e2d24e286b40c7ed7d48ce
gpcoding15 <gisela.pardini15@gmail.com>
2026-06-23 01:38:17 -0300

    all prints removed

--- employee-profile-generator.py
+++ employee-profile-generator.py
@@ -1,8 +1,5 @@
-first_name = 'John'
-last_name = 'Doe'
-full_name = first_name + ' ' + last_name
-address = "123 Main Street"
-print(first_name)
-print(last_name)
-print(full_name)
-print(address)
+first_name = 'John'  
+last_name = 'Doe'  
+full_name = first_name + ' ' + last_name   
+address = '123 Main Street'  
+address += ', Apartment 4B'
